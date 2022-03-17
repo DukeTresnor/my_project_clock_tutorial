772594488faa862365fcaa650b7dc9fde8902b4a
DukeTresnor <armstrongbradford@gmail.com>
2022-03-16 20:27:53 -0700

    Adding changing hands according to current time

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
-    // variable declarations
+    // ** public and serialize field declarations ** //
     [SerializeField]
-    Transform hoursPivot;
+    Transform hoursPivot, minutesPivot, secondsPivot;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // ** private vvariables ** //
+    // Constant variables for conversions from time to degree
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+    
+    // storage variable for time
+    //private DateTime time;
+    private TimeSpan time;
 
-    // Update is called once per frame
+    // Executes when component awakens
     void Update()
     {
-        
+        time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
     }
+
 }
